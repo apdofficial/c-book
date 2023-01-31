fe8a35c5a66938e0e0cb2b9e94c85104cefb3371
Andrej Pištek <andrej.pistek@outlook.com>
2023-01-31 08:10:37 +0100

    ch3

--- src/book.h
+++ src/book.h
@@ -1,10 +1,28 @@
 #ifndef BOOK_H
 #define BOOK_H
 
-#include<stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <limits.h>
 
 #define	FUNCTION_INVOKE_FAILURE	1
 #define	FUNCTION_INVOKE_SUCCESS	0
+
+#define Abs(i) ((i) < 0 ? -(i) : (i))
+#define AbsM(i, flag) ((i) >= 0 ? (i) : ((i)==(flag) ? (flag) : -(i))) //avoid signed integer overflows
+
+/**
+ * Safe conversion
+ * Check the rage of the type into which you are going to cast the given data type.
+ */
+errno_t do_stuff(signed long value) {
+    if ((value < SCHAR_MIN) || (value > SCHAR_MAX)) {
+        return ERANGE;
+    }
+    signed char sc = (signed char) value; // Cast quiets warning
+    return 0;
+}
 
 void my_swap(size_t *a, size_t *b);
 
@@ -13,5 +31,8 @@ void increment(void);
 unsigned int retrieve(void);
 
 int invoke_function(int index);
+
+void print_binary(int n, int len);
+
 
 #endif
