73f8fa2189d01519a097db1a6bdf099f359b2ac5
Andrej Pištek <andrej.pistek@outlook.com>
2023-02-02 08:11:42 +0100

    Finished Chapter 4

--- src/book.c
+++ src/book.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
 #include "book.h"
 
+#include <errno.h>
+#include <limits.h>
+
+
+// static storage
+static int glob = 0;
 static unsigned int counter = 0;
+const static int mLen = 2;
+static int m[mLen] = {1, 2};
+
 
 void my_swap(size_t *a, size_t *b){
     size_t t = *a;
@@ -41,16 +50,14 @@ int invoke_function(int index) {
     return FUNCTION_INVOKE_SUCCESS;
 }
 
-void print_binary(int n, int len)
-{
-    int k = 0;
-    char binary[len];
+void print_binary(int n, int len){
+    printf("The binary representation of %d is \t ", n);
 
-    for (unsigned i = (1 << (len - 1)); i > 0; i = i / 2)
-        binary[k++] = (n & i) ? '1' : '0';
+    unsigned i;
+    for (i = 1 << --len; i > 0; i = i / 2)
+        (n & i) ? printf("1") : printf("0");
 
-    binary[len-1] = '\0';
-    printf("The binary representation of %d is \t %s\n", n, binary);
+    printf("\n");
 }
 
 int cast_value(signed long value) {
@@ -68,4 +75,41 @@ int increment_by_one(int val) {
 
 void invoke_func_and_print(int func(int), int value) {
     printf("%d\n", func(value));
+}
+
+int f(void) {
+    return glob + 10;
+}
+
+int g(void) {
+    glob = 42;
+    return glob;
+}
+
+int max(int a, int b){
+    return a > b ? a: b;
+}
+
+bool is_odd(int n) {
+    // n % 2 == 1 is faulty because reminder sign is determined by the dividend sign, e.g, -n % 2 could be -1 not 1
+    return n % 2 != 0;
+}
+
+bool isN(const int* ptr, int n){
+  return ptr && *ptr == n; // don't dereference a null pointer
+}
+
+int get_m_len(){
+    return mLen;
+}
+
+int* get_m_ptr(void){
+    return m;
+}
+
+int sum_m_elems(void) {
+    int *pi;
+    int j = 0;
+    for (pi = &m[0]; pi < &m[2]; ++pi) j += *pi;
+    return j;
 }
